8df31b1a2356e10b88c7c66bc6e1dc9d0612e512
GBouz <dirxa@hotmail.com>
2022-09-04 20:27:32 +0200

    Implemented run for basic_cleaning and added new dependencies

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -4,6 +4,9 @@ Download from W&B the raw dataset and apply some basic data cleaning, exporting 
 """
 import argparse
 import logging
+import tempfile
+
+import pandas as pd
 import wandb
 
 
@@ -23,6 +26,46 @@ def go(args):
     ######################
     # YOUR CODE HERE     #
     ######################
+    logger.info(f"Fetching dataset '{args.input_artifact}'")
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+    df = pd.read_csv(artifact_local_path)
+
+    # Drop outliers
+    logger.info(f"Dropping outliers")
+    df = df[
+        df['price'].between(args.min_price, args.max_price)
+    ].copy()
+    # Convert last_review to datetime
+    logger.info(f"Converting datetime columns to datetime type")
+    df['last_review'] = pd.to_datetime(df['last_review'])
+
+    # Save the artifacts. We use a temporary directory so we do not leave
+    # any trace behind
+    with tempfile.TemporaryDirectory() as tmp_dir:
+
+        logger.info("Saving to temp folder localy before upload")
+
+        # Get the path on disk within the temp directory
+        temp_path = os.path.join(tmp_dir, args.output_artifact)
+
+        # Save then upload to W&B
+        df.to_csv(temp_path, index=False)
+
+        artifact = wandb.Artifact(
+            name=args.output_artifact,
+            type=args.artifact_type,
+            description=args.output_description,
+        )
+        artifact.add_file(temp_path)
+
+        logger.info(f"Uploading {args.output_artifact} to Weights & Biases")
+        run.log_artifact(artifact)
+
+        # This waits for the artifact to be uploaded to W&B. If you
+        # do not add this, the temp directory might be removed before
+        # W&B had a chance to upload the datasets, and the upload
+        # might fail
+        artifact.wait()
 
 
 if __name__ == "__main__":
